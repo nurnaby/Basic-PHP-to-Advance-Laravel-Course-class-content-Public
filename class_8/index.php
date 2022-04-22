0eaf8fddb72da272122dcb52b77c8941409970cd
nurnaby <sohag.biit@gmail.com>
2022-04-23 05:02:25 +0600

    class 8 class content

--- class_8/index.php
+++ class_8/index.php
@@ -16,9 +16,21 @@
 // htmlspecalchars()
 // strip_tags()
 // trim()
-$string = "I love javascript php ,I love laravel and php  ";
 
-echo strrpos($string,"php");
+// strpos( ) function
+$string1 = "I love javascript php ,I love laravel and php  ";
+echo  strrpos($string1,"php")."<br>";
+
+$string2 = "I love javascript php ,I love laravel and php  ";
+
+echo strpos($string2,"php")."<br>";
+
+$string3 = "I love javascript php ,I love laravel and php  ";
+echo stripos($string3,"PHP")."<br>";
+// substr function()
+$string4 = "I love javascript php ,I love laravel and php";
+echo substr($string4,10);
+
 
 
 
