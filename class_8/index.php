63e36c2a32701e0f8867a2d6d06e0b12afcaeaa7
nurnaby <sohag.biit@gmail.com>
2022-04-24 21:21:37 +0600

    class 8 content

--- class_8/index.php
+++ class_8/index.php
@@ -18,43 +18,43 @@
 // trim()
 
 // strpos( ) function
-$string1 = "I love javascript php ,I love laravel and php  ";
-echo  "strpos".strrpos($string1,"php")."<br>";
+// $string1 = "I love javascript php ,I love laravel and php  ";
+// echo  "strpos".strrpos($string1,"php")."<br>";
 
-$string2 = "I love javascript php ,I love laravel and php  ";
+// $string2 = "I love javascript php ,I love laravel and php  ";
 
-echo "strpos".strpos($string2,"php")."<br>";
+// echo "strpos".strpos($string2,"php")."<br>";
 
-$string3 = "I love javascript php ,I love laravel and php  ";
-echo "stripos".stripos($string3,"PHP")."<br>";
-// substr function()
-$string4 = "I love javascript php ,I love laravel and php";
-echo "substr".substr($string4,2,9);
-// str_splist()
-$string5 = "I love javascript php ";
-echo "<pre>";
-print_r(str_split($string5))."<br>" ;
+// $string3 = "I love javascript php ,I love laravel and php  ";
+// echo "stripos".stripos($string3,"PHP")."<br>";
+// // substr function()
+// $string4 = "I love javascript php ,I love laravel and php";
+// echo "substr".substr($string4,2,9);
+// // str_splist()
+// $string5 = "I love javascript php ";
+// echo "<pre>";
+// print_r(str_split($string5))."<br>" ;
 
 
-// str replsce;
+// // str replsce;
 
-$string6 = "I love javascript php ,I love laravel and php";
-echo "str replace".str_replace("javascript","java",$string6)."<br>";
+// $string6 = "I love javascript php ,I love laravel and php";
+// echo "str replace".str_replace("javascript","java",$string6)."<br>";
 
-// strtoupper function
-echo "shuffle".str_shuffle("Hello World")."<br>";
-// html htmlspecialchars
-$str = "This is some <b>bold</b> text."."<br>";
-echo $str;
-echo "htmlspecialchars".htmlspecialchars($str)."<br>";
+// // strtoupper function
+// echo "shuffle".str_shuffle("Hello World")."<br>";
+// // html htmlspecialchars
+// $str = "This is some <b>bold</b> text."."<br>";
+// echo $str;
+// echo "htmlspecialchars".htmlspecialchars($str)."<br>";
 
-// strip tags()
-echo "strip_tage".strip_tags("Hello <b><i>phpworld!</i></b>","<b>");
-// trim function 
-echo "<p> trim function</p>";
-$str = "Hello World!";
-echo $str . "<br>";
-echo trim($str,"Hed!")."<br>";
+// // strip tags()
+// echo "strip_tage".strip_tags("Hello <b><i>phpworld!</i></b>","<b>");
+// // trim function 
+// echo "<p> trim function</p>";
+// $str = "Hello World!";
+// echo $str . "<br>";
+// echo trim($str,"Hed!")."<br>";
 
 function getAge($date) { // Y-m-d format
     $now = explode("-", date('Y-m-d'));
@@ -76,11 +76,58 @@ function getAge($date) { // Y-m-d format
 }
 
 
-$age= getAge('1988-10-10');
+ 
 
 
-echo"<h1> your age is:$age </h1>";
+if(isset($_POST['submit'])){
+    $age =$_POST['age'];
+   $newAge= getAge($age);
+   
+    
+}
+
+
+// echo"<h1> your age is:$age </h1>";
 
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <form action="" method="post">
+            <div class="haeding">
+                <h2>Check Your BMI</h2>
+            </div>
+            <div class="section1">
+                <span>Enter Date of bath:</span>
+                <input type="date" placeholder="Date of batch" name="age">
+            </div>
+
+            <div class="section3">
+                <input type="submit" name="submit" value="Check BMI">
+                <input type="reset" value="clear">
+            </div>
+            <div class="show">
+                <h2>your age is:<?php echo $newAge;?></h2>
+            </div>
+        </form>
+    </div>
+
+
+</body>
+
+</html>
