aafc8a49c5a0abe54636ae55a517bf75c83038ce
nurnaby <sohag.biit@gmail.com>
2022-07-31 11:07:25 +0600

    update

--- class_10/index.php
+++ class_10/index.php
@@ -29,7 +29,7 @@ function emailValidate($email){
 
 }
 // for edu mail function 
-function fliterEmail($email){
+function fliterEmail($email,$vallidEmails){
     $vallidEmails = ['aub.edu.bd','diu.edu.bd','nsu.edu.bd'];
    $filt_email = explode('@',$email,2);
    return $filt_email[1];
@@ -41,26 +41,80 @@ function fliterEmail($email){
    }
 }
 
-if(isset($_POST['submit'])){
-    $f_name = $_POST['f_name'];
-    $l_name = $_POST['l_name'];
-    $u_email = $_POST['u_email'];
-    $u_password = $_POST['u_password'];
-    $R_u_password = $_POST['R_u_password'];
+// user name validation 
+function validateUsername($username) {
+            
+    
+    if (preg_match('/^\w{5,20}$/', $username)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+// old input value
+function oldinputValue($fieldName){
+    if(isset($_POST[$fieldName])){
+        echo $_POST[$fieldName];
+    }else{
+        echo ' ';
 
-   if(empty($f_name) || empty($l_name) || empty($u_email)){
+    }
+}
+// gender validate 
+// function genderValdate($female){
+//     if()
+
+// }
+
+
+if(isset($_POST['submit'])){
+    $f_name          = $_POST['f_name'];
+    $l_name          = $_POST['l_name'];
+    $u_email         = $_POST['u_email'];
+    $age             = $_POST['age'];
+    
+    $selectCountry   = $_POST['selectCountry'];
+    if(isset($_POST['gender'])){
+        $gender          = $_POST['gender'];
+    }
+    
+    if(isset($_POST['agre_status'])){
+        $agre_status     = $_POST['agre_status'];
+   
+    }
+    
+  
+    
+
+    
+
+   if(empty($f_name) || empty($l_name) || empty($u_email) || empty($age) ||empty($gender)){
 
       $validateMsg= validate("All feild is requar") ;
 
    }else if(emailValidate($u_email)== false){
     $validateMsg= validate('Email is not valid!','warning');
 
-   }else if(fliterEmail($u_email)==false){
+   }else if(fliterEmail($u_email,['aub.edu.bd','diu.edu.bd','nsu.edu.bd'])==false){
 
-    $validateMsg= validate('Email is not enable','warning');
+       $validateMsg= validate('Email is not enable','warning');
+   }else if($age < 18 || $age > 60 ){
+    $validateMsg= validate('your not prefect this course ','warning');
+
+   }else if (validateUsername($f_name) == false) {
+
+    $validateMsg= validate('user name  is not valid','warning');
+
+   }else if($gender=='female'){
+    $validateMsg= validate('you are not anybul this coures','warning');
+
+   }else if($agre_status !='yes'){
+    $validateMsg= validate('you must field trams condition field','warning');
+
    }
    else{
     $validateMsg = validate('Everthing is ok!success','success'); 
+    $_POST= '';
 
    }
     
@@ -99,72 +153,67 @@ if(isset($_POST['submit'])){
             <div class="row px-4 py-2">
 
                 <div class="col-6">
-                    <input type="text" placeholder="Frist Name" class="form-control" name="f_name" autocomplete="off">
+                    <input type="text" placeholder="Frist Name" class="form-control"
+                        value="<?php oldinputValue('f_name') ?>" name="f_name" autocomplete="off">
                     <!-- <?php echo $name_erro ?> -->
                 </div>
                 <div class="col-6">
-                    <input type="text" placeholder="Frist Name" class="form-control" name="l_name" autocomplete="off">
+                    <input type="text" placeholder="Last Name" class="form-control"
+                        value="<?php oldinputValue('l_name') ?>" name="l_name" autocomplete="off">
                 </div>
             </div>
             <div class="row px-4 py-2">
                 <div class="col-12">
-                    <input type="text" placeholder="User Email" class="form-control" name="u_email" autocomplete="off">
+                    <input type="text" placeholder="User Email" class="form-control"
+                        value="<?php oldinputValue('u_email') ?>" name="u_email" autocomplete="off">
                 </div>
             </div>
             <div class="row px-4 py-2">
                 <div class="col-12">
-                    <input type="password" placeholder="password" class="form-control" name="u_password"
-                        autocomplete="off">
+                    <input type="text" placeholder="Enter your  age" class="form-control"
+                        value="<?php oldinputValue('age') ?>" name="age" autocomplete="off">
                 </div>
             </div>
-            <div class="row px-4 py-2 mb-3">
-                <div class="col-12">
-                    <input type="password" placeholder="Re-type password" class="form-control" name="R_u_password"
-                        autocomplete="off">
-                </div>
-            </div>
+
             <div class="row mb-3">
+
+
                 <div class="col margin_left">
+                    <label for="">Gender</label><br>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
-                            value="male">
+                        <input class="form-check-input" type="radio" name="gender" id="inlineRadio1" value="male">
                         <label class="form-check-label" for="inlineRadio1">Male</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2"
-                            value="female">
+                        <input class="form-check-input" type="radio" name="gender" id="inlineRadio2" value="female">
                         <label class="form-check-label" for="inlineRadio2">Female</label>
                     </div>
                 </div>
             </div>
             <div class="row px-4 mb-4">
                 <div class="col-12 ">
-                    <select class="form-control">
+                    <select class="form-control" name="selectCountry">
                         <option>select country </option>
-                        <option>Bangladesh </option>
-                        <option>Pakistan </option>
+                        <option value="Bangladesh">Bangladesh </option>
+                        <option value="Pakistan">Pakistan </option>
                     </select>
                 </div>
             </div>
             <div class="row mb-4">
                 <dic class="col margin_left">
                     <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                        <input class="form-check-input" type="checkbox" name="agre_status" value="yes"
+                            id="flexCheckDefault">
                         <label class="form-check-label" for="flexCheckDefault">
                             I agre with terms and conditions
                         </label>
                     </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-                        <label class="form-check-label" for="flexCheckChecked">
-                            I agre with terms with condition
-                        </label>
-                    </div>
+
                 </dic>
             </div>
 
             <div class="row px-4 ">
-                <div class="col-12 mb-4">
+                <div class="col-12 mb-2">
                     <!-- <a href=" " class="btn btn-warning btn-block " name="submit">Submit</a> -->
                     <button class="btn btn-warning btn-block" name="submit">submit</button>
                 </div>
